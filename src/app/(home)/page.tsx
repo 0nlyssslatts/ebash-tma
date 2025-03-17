4feb328a42afb9d2fe00e9e7a7318d97cda5e198
Justifall <stasepik437@gmail.com>
2025-03-18 00:01:53 +0300

    init

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+import Home from './Home';
+
+export const metadata: Metadata = {
+    title: 'Главная',
+    description: 'Главная страница',
+};
+
+export default async function HomePage() {
+    return <Home />;
+}
