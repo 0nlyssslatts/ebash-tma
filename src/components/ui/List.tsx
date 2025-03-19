b4f42baf7988d89a8ff681b2bfcac7517803bbff
0nlyssslatts <sid36205@mail.ru>
2025-03-19 12:56:11 +0300

    lint fix

--- src/components/ui/List.tsx
+++ src/components/ui/List.tsx
@@ -4,7 +4,7 @@ export function List({ listArray }: { listArray: string[] }) {
   return (
     <ul className="flex flex-col gap-2">
       {listArray.map((item: string, index: number) => {
-        return <ListItem index={index + 1} item={item} />;
+        return <ListItem key={index} index={index + 1} item={item} />;
       })}
     </ul>
   );
