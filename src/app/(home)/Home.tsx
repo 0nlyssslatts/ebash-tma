804c7e695d8fa7ff2ac8faad9debc5a18092d7cd
0nlyssslatts <sid36205@mail.ru>
2025-03-19 09:51:43 +0300

    Page ui component added

--- src/app/(home)/Home.tsx
+++ src/app/(home)/Home.tsx
@@ -1,30 +1,29 @@
 'use client';
 
 import { Button } from '@/components/ui/Button';
-import { Header } from "@/components/ui/Header";
 import { DarkSection } from '@/components/ui/DarkSection';
+import { Header } from '@/components/ui/Header';
+import { Page } from '@/components/ui/Page';
 
 export default function Home() {
   return (
-    <div className="page">
-      <div className="flex flex-col justify-center items-center max-w-2xl mx-auto p-4">
-        <Header text='EBASH' />
-        <DarkSection>
-          <p className="text-lg">Здарова, че бакланишь?</p>
-          <p className="text-sm mt-2">
-            Мы сообщество тех, кто стремится к саморазвитию, ответственности за свою жизнь и осознанному подходу к ней.
-            Не просто работаем, а вкладываемся на полную во все сферы жизни: от работы и учёбы до отдыха и личного роста
-          </p>
-          <div className="flex flex-col gap-2 mt-4">
-            <Button text="Гайд новичка" />
-            <Button text="Календарь" />
-            <Button text="Чаты" />
-            <Button text="Регулярные активности" />
-            <Button text="Ништяки и материалы" />
-            <Button text="Написать куратору" />
-          </div>
-        </DarkSection>
-      </div>
-    </div>
+    <Page>
+      <Header text="EBASH" />
+      <DarkSection>
+        <p className="text-lg">Здарова, че бакланишь?</p>
+        <p className="text-sm mt-2">
+          Мы сообщество тех, кто стремится к саморазвитию, ответственности за свою жизнь и осознанному подходу к ней. Не
+          просто работаем, а вкладываемся на полную во все сферы жизни: от работы и учёбы до отдыха и личного роста
+        </p>
+        <div className="flex flex-col gap-2 mt-4">
+          <Button text="Гайд новичка" />
+          <Button text="Календарь" />
+          <Button text="Чаты" />
+          <Button text="Регулярные активности" />
+          <Button text="Ништяки и материалы" />
+          <Button text="Написать куратору" />
+        </div>
+      </DarkSection>
+    </Page>
   );
 }
