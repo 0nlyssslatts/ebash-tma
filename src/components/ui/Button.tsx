a806a9171843f4cf2da9becd17d9b70b73a69dda
0nlyssslatts <sid36205@mail.ru>
2025-03-19 09:15:11 +0300

    tailwind config base colors added

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,7 +12,7 @@ export function Button({
       onClick={onClick}
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
-      className="w-full hover:cursor-pointer hover:bg-[#96FFCE0D] bg-[#282828] py-3 rounded font-medium transition-all relative"
+      className="w-full hover:cursor-pointer hover:bg-backgroundgreen bg-background text-textgreen py-3 rounded font-medium transition-all relative"
     >
       {text}
     </motion.button>
