2a62b1df85b4887514b6055dcdd660bdeba4beb3
0nlyssslatts <sid36205@mail.ru>
2025-03-19 11:08:32 +0300

    routing added

--- src/app/(home)/Home.tsx
+++ src/app/(home)/Home.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { Button } from '@/components/ui/Button';
 import { DarkSection } from '@/components/ui/DarkSection';
 import { Header } from '@/components/ui/Header';
@@ -18,12 +20,24 @@ export default function Home() {
           просто работаем, а вкладываемся на полную во все сферы жизни: от работы и учёбы до отдыха и личного роста
         </TextLight>
         <div className="flex flex-col gap-2 mt-4">
-          <Button text="Гайд новичка" />
-          <Button text="Календарь" />
-          <Button text="Чаты" />
-          <Button text="Регулярные активности" />
-          <Button text="Ништяки и материалы" />
-          <Button text="Написать куратору" />
+          <Link href="/guide">
+            <Button text="Гайд новичка" />
+          </Link>
+          <Link href="/meets">
+            <Button text="Календарь" />
+          </Link>
+          <Link href="/chats">
+            <Button text="Чаты" />
+          </Link>
+          <Link href="/moves">
+            <Button text="Регулярные активности" />
+          </Link>
+          <Link href="/bonus">
+            <Button text="Ништяки и материалы" />
+          </Link>
+          <Link href="/help">
+            <Button text="Написать куратору" />
+          </Link>
         </div>
       </DarkSection>
     </Page>
