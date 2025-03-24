dc23ef4485ff8e629f410c3fad2ad27c612dd647
Justifall <84130430+Angst04@users.noreply.github.com>
2025-03-24 17:02:25 +0300

    Добавлена страницы Chats

---
+++ src/components/ChatCard.tsx
@@ -0,0 +1,25 @@
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+import { TextLight } from './ui/TextLight';
+
+const colors = ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-purple-500', 'bg-pink-500'];
+
+export function ChatCard({ title, text, index, href }: { title: string; text: string; index: number; href: string }) {
+  const color = colors[index % colors.length];
+
+  return (
+    <Link href={href} className="block">
+      <motion.div
+        whileHover={{ scale: 1.02, y: -8 }}
+        whileTap={{ scale: 0.98 }}
+        className="overflow-hidden relative rounded px-5 py-7 w-full flex flex-col gap-2 border"
+      >
+        <p className="font-blackout text-6xl m-3 text-center">{title}</p>
+        <TextLight className="">{text}</TextLight>
+
+        <div className={`w-full h-3 ${color} absolute bottom-0 left-0`}></div>
+      </motion.div>
+    </Link>
+  );
+}
--- src/components/ui/DarkSection.tsx
+++ src/components/ui/DarkSection.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
 export function DarkSection({ children }: { children: ReactNode }) {
-  return <div className="bg-secondary rounded px-5 py-3 w-[100%]">{children}</div>;
+  return <div className="bg-secondary rounded px-5 py-3 w-full">{children}</div>;
 }
