ef2466bd573d5bfd4e99ff49ff04eb0612650c85
0nlyssslatts <sid36205@mail.ru>
2025-03-31 19:22:01 +0300

    Добавил попап чата

--- src/components/Cards/ChatCard.tsx
+++ src/components/Cards/ChatCard.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Link from 'next/link';
+import { useState } from 'react';
 
 import { TextLighter } from '../ui/Texts/TextLighter';
+
+import { ChatPopup } from './ChatPopup';
 
 interface CardProps {
   title: string;
@@ -13,19 +15,25 @@ interface CardProps {
   color: string;
 }
 
-export function ChatCard({ title, descriptionShort, href, color }: CardProps) {
+export function ChatCard({ title, descriptionShort, descriptionLong, href, color }: CardProps) {
+  const [showPopup, setShowPopup] = useState(false);
+  const handlePopup = () => setShowPopup((prev) => !prev);
   return (
-    <Link href={href} className="block">
+    <>
       <motion.div
         whileHover={{ scale: 1.02, y: -8 }}
         whileTap={{ scale: 0.98 }}
-        className="overflow-hidden z-1 relative rounded px-2 py-2 w-full flex flex-col gap-2 border"
+        className="overflow-hidden relative rounded px-2 py-2 w-full flex flex-col gap-2 border"
+        onClick={handlePopup}
       >
         <p className="font-blackout text-4xl md:text-6xl text-center">{title}</p>
         <TextLighter>{descriptionShort}</TextLighter>
 
         <div style={{ backgroundColor: `${color}` }} className={`w-full h-2 z-3 absolute bottom-0 left-0`}></div>
       </motion.div>
-    </Link>
+      {showPopup && (
+        <ChatPopup title={title} descriptionLong={descriptionLong} href={href} popupHandler={handlePopup} />
+      )}
+    </>
   );
 }
