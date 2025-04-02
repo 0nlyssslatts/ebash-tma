529f241e46333fa752774d6c0ae74443e625aeb7
0nlyssslatts <sid36205@mail.ru>
2025-04-02 22:22:14 +0300

    bonus page added

--- src/app/(withMenuNavigation)/bonus/page.tsx
+++ src/app/(withMenuNavigation)/bonus/page.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from 'next';
+import { groq } from 'next-sanity';
 
 import Bonus from './Bonus';
+import { client } from '@/sanity/client';
 
 export const metadata: Metadata = {
   title: 'Ништяки',
   description: 'Ништяки и материалы',
 };
 
+export const revalidate = 43200;
+
+async function getBonusPageData() {
+  return await client.fetch(groq`*[_type == "bonusPage"][0]{
+    header, 
+    "bonuses": bonuses[] { title, description, buttonDescription, buttonText, buttonHref }
+  }`);
+}
+
 export default async function BonusPage() {
-  return <Bonus />;
+  const data = await getBonusPageData();
+  return <Bonus {...data} />;
 }
